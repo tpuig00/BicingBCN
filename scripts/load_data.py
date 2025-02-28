66252e7c4cad90b6af2bd7dad28ddc78ce8261bd
maximilianparadis <maxschaferparadis@gmail.com>
2025-02-28 19:04:07 +0100

    Update load_data.py
    Primera idea, recordeu modificar l'extraccio de dades de "data_download" amb nomes els anys 2024 I 2025. Cada dataset té 4 milions d'observacions. Si volem un dataset Unic només podem agafar aquestes dates.

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,2 +1,37 @@
 def cargar_datos():
-    return "datos cargados"
+    import os
+    import pandas as pd  #aquesta part de moment la poso aquí però, potser hauria d'anar al main (ho poso per si algu no les carrega)
+    data_path = './data'
+    files = os.listdir(data_path)
+    
+    df_list = []
+
+    for file in files:
+        if file.endswith('.csv'):  
+            file_path = os.path.join(data_path, file)
+
+            parts = file.split('_')
+            if len(parts) < 3:
+                continue
+            
+            year = parts[0]  
+            month_name = parts[2]  
+
+            try:
+                df = pd.read_csv(file_path, low_memory=False, dtype=str)
+
+                df['last_reported'] = pd.to_datetime(df['last_reported'], unit='s', errors='coerce')
+                df['last_updated'] = pd.to_datetime(df['last_updated'], unit='s', errors='coerce')
+
+                df['Year'] = year
+                df['Month'] = month_name
+
+                df_list.append(df)
+
+            except Exception:
+                continue
+
+    return pd.concat(df_list, ignore_index=True) if df_list else None
+
+df = cargar_datos()
+
